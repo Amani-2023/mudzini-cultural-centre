cd0625bf3de4c50110b6aae385a8820d899240d0
amani-2023 <amanikadenge13@gmail.com>
2025-10-10 13:02:57 +0300

    Updated all the files

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
-<<<<<<< HEAD
+// script.js
 document.addEventListener("DOMContentLoaded", () => {
-    document.querySelectorAll("img").forEach(img => {
-        img.addEventListener("load", () => img.classList.add("loaded"));
+    const currentPage = window.location.pathname.split("/").pop();
+    const navLinks = document.querySelectorAll("nav ul li a");
+
+    navLinks.forEach(link => {
+        const linkPage = link.getAttribute("href");
+        if (linkPage === currentPage || (linkPage === "index.html" && currentPage === "")) {
+            link.classList.add("active");
+        } else {
+            link.classList.remove("active");
+        }
     });
-=======
-document.addEventListener("DOMContentLoaded", () => {
-    document.querySelectorAll("img").forEach(img => {
-        img.addEventListener("load", () => img.classList.add("loaded"));
-    });
->>>>>>> b01870eb6b259c6edefc036c6765283c12143281
 });
